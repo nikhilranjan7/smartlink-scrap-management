6bf82faad3c6e9bca713a25ebe6042249ce644af
nikhilranjan7 <nikhilranjan7@gmail.com>
2017-05-24 10:34:03 +0530

    Add Models.py

--- scrap/scr/models.py
+++ scrap/scr/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+Department = (
+    ('rma','RMA'),
+    ('production', 'PRODUCTION'),
+    ('warehouse', 'WAREHOUSE'),
+    ('maintenance', 'MAINTENANCE'),
+)
+
+Type = (
+    ('hazardous', 'HAZARDOUS')
+    ('e-waster', 'E-WASTE')
+    ('metals', 'METALS')
+    ('paper', 'PAPER')
+    ('plastic', 'PLASTIC')
+)
+
+class Category(models.Model):
+  department = models.CharField(max_length=14,                                                                 choices=COLOR_CHOICES,
+                 default='rma')
+  waste_type = models.CharField(max_length=14, default='paper')
+  description = models.CharField(max_length=100)
+
+  def __str__(self):
+      return self.name
