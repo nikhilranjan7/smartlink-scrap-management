121654d9dded3d1e5b1acbec3881afc2f9874d5c
nikhilranjan7 <nikhilranjan7@gmail.com>
2017-06-02 12:04:43 +0530

    Y

--- scrap/scr/urls.py
+++ scrap/scr/urls.py
@@ -4,5 +4,5 @@ from scr import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
 	url(r'^add/$', views.add, name='add'),
-	url(r'^quote/$', views.quotes, name='quote')
+	url(r'^quote/$', views.quotes, name='quota'),
 ]
