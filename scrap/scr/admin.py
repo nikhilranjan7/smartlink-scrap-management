1a8bdfa3ff84fd8a53e1e7229be22bc88d426878
Ahmed Elemam <eMaM151987@gmail.com>
2017-07-09 21:26:39 +0200

    Update admin.py

--- scrap/scr/admin.py
+++ scrap/scr/admin.py
@@ -1,29 +1,29 @@
 from django.contrib import admin
-from scr.models import Category, Quote, trxn_m
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin
 
+from models import Category, Quote, trxn_m
 
 
 class CategorylistResource(resources.ModelResource):
-
     class Meta:
         model = Category
+
 
 class CategoriesAdmin(ImportExportModelAdmin):
     resource_class = CategorylistResource
 
 
 class QuotelistResource(resources.ModelResource):
-
     class Meta:
         model = Quote
+
 
 class QuoteListAdmin(ImportExportModelAdmin):
     resource_class = QuotelistResource
 
-class trxlistResource(resources.ModelResource):
 
+class trxlistResource(resources.ModelResource):
     class Meta:
         model = trxn_m
 
@@ -32,6 +32,6 @@ class trxListAdmin(ImportExportModelAdmin):
     resource_class = trxlistResource
 
 
-admin.site.register(Category,CategoriesAdmin)
-admin.site.register(Quote,QuotelistResource)
-admin.site.register(trxn_m,trxListAdmin)
+admin.site.register(Category, CategoriesAdmin)
+admin.site.register(Quote,QuoteListAdmin)
+admin.site.register(trxn_m, trxListAdmin)
